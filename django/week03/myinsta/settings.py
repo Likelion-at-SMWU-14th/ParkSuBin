d1e781b6fb4865e53110522bd4b692070f203cb8
subin1230 <parksubin@sookmyung.ac.kr>
2026-05-24 21:56:06 +0900

    이어서는 과제에서 실행

--- django/week03/myinsta/settings.py
+++ django/week03/myinsta/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'blog',
+    'users',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
