618a9698fdbc85ef8b47d5d9579ce21804cbca61
박수빈 <parksubin@bagsubin-ui-MacBookPro.local>
2026-09-08 20:52:50 +0900

    Feat: 영속성 전이 실습

--- spring/week06/seminar/src/main/java/com/likelion/seminar/entity/Provider.java
+++ spring/week06/seminar/src/main/java/com/likelion/seminar/entity/Provider.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.w3c.dom.html.HTMLSelectElement;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +23,14 @@ public class Provider extends BaseEntity{
         this.name = name;
     }
 
-    @OneToMany(mappedBy = "provider")
+    @OneToMany(mappedBy = "provider", cascade = CascadeType.PERSIST)
     @ToString.Exclude
     private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product){
+        products.add(product);
+        product.setProvider(this);
+    }
 }
+
+
